d343b15f1bca76b7e752d9357893197690e2ef87
Terrance Fisher <teefisher314@gmail.com>
2021-04-02 19:07:55 -0500

    Annotations and validation of Dog project

--- src/main/java/com/practicecoding/practicecoding/controllers/DogController.java
+++ src/main/java/com/practicecoding/practicecoding/controllers/DogController.java
@@ -6,20 +6,26 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 
 @Controller
 public class DogController {
 
     @GetMapping("dog/create")
-    public String createDogPage() {
+    public String createDogPage(Model model) {
+        model.addAttribute("dog", new Dog());
         return "dog";
     }
 
     @PostMapping("dog/create")
-    public String createADog(@ModelAttribute Dog myDog) {
+    public String createADog(@ModelAttribute @Valid Dog myDog, Error errors) {
+        //if (errors.hasErrors()) is correct but throws an error needs fixing
+        if (errors.equals(true)) {
+            return "dog";
+        }
         DogData.getMyDogs().add(myDog);
-        return "redirect:/show";
+        return "show-one-dog";
 
     }
 
--- src/main/java/com/practicecoding/practicecoding/models/Dog.java
+++ src/main/java/com/practicecoding/practicecoding/models/Dog.java
@@ -1,12 +1,25 @@
 package com.practicecoding.practicecoding.models;
 
-public class Dog {
+import org.jetbrains.annotations.NotNull;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class Dog {
+    @NotBlank(message = "Dog name can't be blank!" )
+    @NotNull
+    @Size(min=3, message="Needs to be longer")
     private String name;
+
+    @Min(value = 1, message = "Cannot be lower than 0")
     private int age;
 
+    public Dog() {
+        this("",0);
+    }
+//NotBlank and @NotNull are Annotations that prevent the user from returning a blank name
     public Dog(String name, int age) {
-
         this.name = name;
         this.age = age;
     }
